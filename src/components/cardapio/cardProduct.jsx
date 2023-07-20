7e109068baf391700b041106bfb1b9d566c982bc
devHonorio <honorio.dev@gmail.com>
2023-07-19 21:44:01 -0300

    animation menu

--- src/components/cardapio/cardProduct.jsx
+++ src/components/cardapio/cardProduct.jsx
@@ -1,12 +1,14 @@
 export default function CardProduct({ title, value, image }) {
 	return (
-		<div className='bg-white flex flex-col basis-28 flex-grow  h-48  md:h-72 md:basis-52 p-3 rounded-3xl shadow-sm justify-between items-center hover:bg-orange-400 cursor-pointer md:flex-grow-0 '>
+		<div className='group bg-white flex flex-col basis-28 flex-grow  h-48  md:h-72 md:basis-52 p-3 rounded-3xl shadow-sm justify-between items-center hover:bg-orange-400 cursor-pointer md:flex-grow-0 '>
 			<div className='bg-[url("/bolos/recheios/brigadeiro.jpg")] bg-cover w-full h-48 rounded-3xl mb-2 '></div>
-			<h2 className='font-semibold text-xs md:text-base '>{title}</h2>
+			<h2 className='font-semibold text-xs text-center mb-1 leading-3 md:text-base '>
+				{title}
+			</h2>
 			<div className='flex '>
 				<button className='hidden'>+</button>
 				<button className='hidden'>-</button>
-				<p className='font-semibold text-orange-500 text-xs md:text-base'>
+				<p className='font-semibold text-orange-500 text-xs md:text-base group-hover:text-white group-hover:font-extrabold'>
 					{value}
 				</p>
 			</div>
--- src/components/global/Navigation.jsx
+++ src/components/global/Navigation.jsx
@@ -5,8 +5,8 @@ export default function Navigation({ isOpen, setIsOpen }) {
 	return (
 		<nav
 			className={`${
-				!isOpen && 'hidden'
-			} items-center w-full absolute top-0 left-0 right-0 bottom-0 bg-white flex flex-col justify-center gap-5 z-20 lg:flex lg:justify-around lg:static lg:bg-inherit lg:flex-row `}>
+				!isOpen && ' hidden '
+			} animate-fade-up animate-once animate-duration-500 animate-ease-in-out  items-center w-full absolute top-0 left-0 right-0 bottom-0 bg-white flex flex-col justify-center gap-5 z-20 lg:flex lg:justify-around lg:static lg:bg-inherit lg:flex-row `}>
 			<button
 				className='lg:hidden absolute top-7 right-7'
 				onClick={() => setIsOpen(!isOpen)}>
