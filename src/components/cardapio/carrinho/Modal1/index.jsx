199deb48e9553e2c92b9d07ea30f5b03439d5909
devHonorio <honorio.dev@gmail.com>
2023-09-06 12:13:10 -0300

    remove contextCarrinho

--- src/components/cardapio/carrinho/Modal1/index.jsx
+++ src/components/cardapio/carrinho/Modal1/index.jsx
@@ -1,26 +1,14 @@
 import Button from '@/components/comum/Button'
 import ItemCarrinho from '../ItemCarrinho'
 import Footer from '../Footer'
-import { useCarrinho } from '@/components/contexts/ContextCarrinho'
+
 import { useId } from 'react'
 import ItemBolo from './ItemBolo'
 
 export default function Modal1({ className, setIsActive }) {
-	const { carrinho } = useCarrinho()
-	const bolos = carrinho.filter((e) => e.type === 'bolo')
 	return (
 		<div className={`${className}`}>
-			<div className='flex flex-col divide-y-2 pb-4'>
-				{bolos.map((e) => {
-					const id = useId()
-					return <ItemBolo peso={e.peso} recheios={e.recheios} key={id} />
-				})}
-				{carrinho.map((e) => {
-					const id = useId()
-					const recheios = e.recheios.map((e) => e.name + ' ')
-					return <ItemCarrinho key={id} peso={e.peso} title={recheios} />
-				})}
-			</div>
+			<div className='flex flex-col divide-y-2 pb-4'></div>
 
 			<Footer />
 
