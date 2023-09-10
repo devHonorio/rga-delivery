265029046aa9ec2757930b3f9a1d23db7066c0a1
devHonorio <honorio.dev@gmail.com>
2023-09-10 03:46:34 -0300

    correçao bug carrinho localStorage

--- src/components/cardapio/carrinho/Layout/index.jsx
+++ src/components/cardapio/carrinho/Layout/index.jsx
@@ -2,14 +2,19 @@ import { useCarrinho } from '@/hooks/useStorage'
 import FooterCarrinho from './FooterCarrinho'
 import HeaderCarrinho from './HeaderCarrinho'
 import { SContainer, SHmin } from './styles'
+import { useEffect, useState } from 'react'
 
 export default function LayoutCarrinho({ children, className, isActive }) {
+	const [carrinho, setCarrinho] = useState()
 	const { getStorage } = useCarrinho()
+	useEffect(() => {
+		setCarrinho(getStorage())
+	}, [])
 	return (
 		<SContainer className={className}>
 			<HeaderCarrinho isActive={isActive} />
-			{!getStorage() && 'Não á itens no carrinho'}
-			{getStorage() && children}
+			{!carrinho && 'Não á itens no carrinho'}
+			{carrinho && children}
 		</SContainer>
 	)
 }
