44a0bc1aed3f674983ec4da26b8fb1a01d7a49e2
devHonorio <honorio.dev@gmail.com>
2023-09-18 10:55:51 -0300

    ajustes entrega

--- src/hooks/useMensage.js
+++ src/hooks/useMensage.js
@@ -8,17 +8,17 @@ export const useMensage = () => {
 	const date = `${carrinho.date}`
 
 	let bolos = ''
-
-	carrinho.bolos?.forEach(
-		(e) =>
-			(bolos += `${e.bento ? 'Bento' : e.peso + 'kg'} ${e.recheios.map(
-				(e) => ' ' + e.name
-			)} ${e.priceBolo?.toLocaleString('pt-BR', {
-				style: 'currency',
-				currency: 'BRL',
-			})}
-${e.formato.toUpperCase()} \n\n\n`)
-	)
+	let bolosPrice = 0
+	carrinho.bolos?.forEach((e) => {
+		bolosPrice += +e.priceBolo
+		bolos += `${e.bento ? 'Bento' : e.peso + 'kg'} ${e.recheios.map((e) => {
+			' ' + e.name
+		})} ${e.priceBolo?.toLocaleString('pt-BR', {
+			style: 'currency',
+			currency: 'BRL',
+		})}
+${e.formato.toUpperCase()} \n\n\n`
+	})
 
 	let docesTradicionais = ''
 	let docesTradicionaisPrice = 0
@@ -30,6 +30,7 @@ ${e.formato.toUpperCase()} \n\n\n`)
 	const entrega = !carrinho.entrega
 		? 'Retirada no local'
 		: `${carrinho.entrega.rua} - ${carrinho.entrega.number}, ${carrinho.entrega.bairro}, ${carrinho.entrega.cidade}`
+	let taxa = carrinho.entrega ? 5 : 0
 	const message = `*${name} ${date?.split('-').reverse().join('/')}*
 
 
@@ -37,7 +38,7 @@ ${bolos}
       
 ${
 	docesTradicionais +
-	'*Subtotal..............' +
+	'*Doces tradicionais..............' +
 	docesTradicionaisPrice?.toLocaleString('pt-BR', {
 		style: 'currency',
 		currency: 'BRL',
@@ -49,8 +50,11 @@ ${carrinho.entrega ? '*Taxa de entrega..............R$ 5,00*' : ''}
 
 ${carrinho.observacoes ? '*' + carrinho.observacoes + '*' : ''}
 
-*${entrega}* 
-      `
+*Total...............${bolosPrice + docesTradicionaisPrice + taxa}
+
+*${entrega}* `
+
+	console.log(message)
 	return encodeURI(message)
 	//
 }
