a28713cb5f88c82bc9cbaeabcfcf05f810fcff44
devHonorio <honorio.dev@gmail.com>
2023-10-02 12:06:36 -0300

    ajustando mensage para receber todos produtos

--- src/hooks/useMensage.js
+++ src/hooks/useMensage.js
@@ -11,13 +11,13 @@ export const useMensage = () => {
 	let bolos = ''
 	let bolosPrice = 0
 	carrinho.bolos?.forEach((e) => {
+		const boloPeso = e.bento ? 'Bento' : e.peso + 'kg'
+		const boloRecheios = e.recheios.map((e) => ' ' + e.name)
+		const boloPrice = useFormatToRealBRL(e.priceBolo)
+		const boloFormato = e.formato.toUpperCase()
+		const boloObservacoes = e.observacoesBolo ? e.observacoesBolo : 'Nenhuma'
 		bolosPrice += +e.priceBolo
-		bolos += `${e.bento ? 'Bento' : e.peso + 'kg'} ${e.recheios.map(
-			(e) => ' ' + e.name
-		)} ${useFormatToRealBRL(e.priceBolo)}
-${e.formato.toUpperCase()}
-
-*Observações:* ${e.observacoesBolo ? e.observacoesBolo : 'Nenhuma'} \n\n\n`
+		bolos += `${boloPeso} ${boloRecheios} ${boloPrice} \n${boloFormato} \nObservações: ${boloObservacoes} \n\n\n`
 	})
 
 	let docesTradicionais = ''
@@ -27,11 +27,46 @@ ${e.formato.toUpperCase()}
 		docesTradicionaisPrice += e.priceTotal
 	})
 
+	let bolosDeForma = ''
+	let bolosDeFormaPrice = 0
+	carrinho.bolosDeForma?.forEach((e) => {
+		bolosDeForma += `${e.quantity} ${e.title} \n`
+		bolosDeFormaPrice += e.priceTotal
+	})
+
+	let docesGourmet = ''
+	let docesGourmetPrice = 0
+	carrinho.docesGourmet?.forEach((e) => {
+		docesGourmet += `${e.quantity} ${e.title} \n`
+		docesGourmetPrice += e.priceTotal
+	})
+
+	let docesFinos = ''
+	let docesFinosPrice = 0
+	carrinho.docesFinos?.forEach((e) => {
+		docesFinos += `${e.quantity} ${e.title} \n`
+		docesFinosPrice += e.priceTotal
+	})
+
 	let salgados = ''
 	let salgadosPrice = 0
 	carrinho.salgados?.forEach((e) => {
 		salgados += `${e.quantity} ${e.title} \n`
 		salgadosPrice += e.priceTotal
+	})
+
+	let frios = ''
+	let friosPrice = 0
+	carrinho.frios?.forEach((e) => {
+		frios += `${e.quantity} ${e.title} \n`
+		friosPrice += e.priceTotal
+	})
+
+	let bebidas = ''
+	let bebidasPrice = 0
+	carrinho.bebidas?.forEach((e) => {
+		bebidas += `${e.quantity} ${e.title} \n`
+		bebidasPrice += e.priceTotal
 	})
 
 	const entrega = !carrinho.entrega
@@ -54,8 +89,33 @@ ${
 }
 
 ${
+	docesGourmet &&
+	docesGourmet +
+		'*Doces gourmet..............' +
+		useFormatToRealBRL(docesGourmetPrice) +
+		'*'
+}
+
+${
+	docesFinos &&
+	docesFinos +
+		'*Doces finos..............' +
+		useFormatToRealBRL(docesFinosPrice) +
+		'*'
+}
+
+${
 	salgados &&
 	salgados + '*Salgados..............' + useFormatToRealBRL(salgadosPrice) + '*'
+}
+
+${
+	frios && frios + '*Frios..............' + useFormatToRealBRL(friosPrice) + '*'
+}
+
+${
+	bebidas &&
+	bebidas + '*Bebidas..............' + useFormatToRealBRL(bebidasPrice) + '*'
 }
 
 ${carrinho.entrega ? '*Taxa de entrega..............R$ 5,00*' : ''}
@@ -63,7 +123,15 @@ ${carrinho.entrega ? '*Taxa de entrega..............R$ 5,00*' : ''}
 ${carrinho.observacoes ? '*' + carrinho.observacoes + '*' : ''}
 
 *Total...............${useFormatToRealBRL(
-		bolosPrice + docesTradicionaisPrice + salgadosPrice + taxa
+		bolosPrice +
+			bolosDeFormaPrice +
+			docesTradicionaisPrice +
+			docesGourmetPrice +
+			docesFinosPrice +
+			salgadosPrice +
+			friosPrice +
+			bebidasPrice +
+			taxa
 	)}*
 
 *${entrega}*  _*${time}*_`
