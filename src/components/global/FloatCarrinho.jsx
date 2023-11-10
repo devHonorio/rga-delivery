26914e34531541a0c5ae3050d07bf8b629891ff9
devHonorio <honorio.dev@gmail.com>
2023-11-10 09:10:54 -0300

    Ajusta botão flutuante

--- src/components/global/FloatCarrinho.jsx
+++ src/components/global/FloatCarrinho.jsx
@@ -1,19 +1,19 @@
 import Link from 'next/link'
 import * as S from './styles'
-import { Bag } from '../comum/Icons'
+import { Cart } from '../comum/Icons'
 
 export default function FloatCarrinho() {
-	return (
-		<Link
-			href={'carrinho'}
-			onClick={() => {
-				sessionStorage.clear()
-			}}>
-			<div className='fixed right-8 bottom-10 z-50'>
-				<S.FloatCarrinho>
-					<Bag className='h-5 w-5' />
-				</S.FloatCarrinho>
-			</div>
-		</Link>
-	)
+  return (
+    <Link
+      href={'carrinho'}
+      onClick={() => {
+        sessionStorage.clear()
+      }}>
+      <div className='fixed right-8 bottom-10 z-50'>
+        <S.FloatCarrinho>
+          <Cart className='h-6 w-6' />
+        </S.FloatCarrinho>
+      </div>
+    </Link>
+  )
 }
