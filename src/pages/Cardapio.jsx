59ef9bd3ca0b362db2db2ea15fa646d3f57b86f9
José Honorio de Oliveira Sobrinho Neto <107776469+devHonorio@users.noreply.github.com>
2024-07-01 14:08:34 +0000

    em manutenção

--- src/pages/Cardapio.jsx
+++ src/pages/Cardapio.jsx
@@ -1,10 +1,11 @@
-import CardapioIndex from "@/components/cardapio";
+// import CardapioIndex from "@/components/cardapio";
 import Layout from "@/components/global/Layout";
 
 export default function Cardapio() {
   return (
     <Layout>
-      <CardapioIndex />
+      <h1>Em Manutenção</h1>
+      {/*<CardapioIndex />*/}
     </Layout>
   );
 }
